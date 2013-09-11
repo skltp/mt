6ba4d08b507f6eaa1b8f33275922ceb6674181ac
Mats Olsson <mats.olsson@callistaenterprise.se>
2013-09-11 12:39:49 +0200

    ... now actually deleted the old files

--- composites/schemas/src/main/generated/se/skltp/mt/qa/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:riv:insuranceprocess:healthreporting:medcertqa:1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.skltp.mt.qa.v1;
