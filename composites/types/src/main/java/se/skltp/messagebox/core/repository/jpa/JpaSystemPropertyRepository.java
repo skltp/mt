d1bb7331650900e268b4dfed7b5cdf8edc50f7c5
Mats Olsson <mats.olsson@callistaenterprise.se>
2013-10-09 12:41:16 +0200

    INFRA-86: Copied license management from Certificate project and updated files

--- composites/types/src/main/java/se/skltp/messagebox/core/repository/jpa/JpaSystemPropertyRepository.java
+++ composites/types/src/main/java/se/skltp/messagebox/core/repository/jpa/JpaSystemPropertyRepository.java
@@ -1,22 +1,20 @@
-/*
- * Copyright 2010 Inera
+/**
+ * Copyright (C) 2013 Inera AB (http://www.inera.se)
  *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of version 2.1 of the GNU Lesser General Public
+ * This file is part of Inera MessageService (http://code.google.com/p/inera-message).
  *
- *   License as published by the Free Software Foundation.
+ * Inera MessageService is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   This library is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Inera MessageService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   GNU Lesser General Public License for more details.
- *
- *   You should have received a copy of the GNU Lesser General Public
- *   License along with this library; if not, write to the
- *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- *
- *   Boston, MA 02111-1307  USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package se.skltp.messagebox.core.repository.jpa;
 
